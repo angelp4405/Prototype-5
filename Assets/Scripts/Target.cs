1ec54c8df6d24e1e4e547575629611e743b71cb7
angel <avasquez27@aoiths.org>
2026-02-10 12:13:40 -0500

    Create w/ Code Part 5.2

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -10,6 +10,7 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -2;
+    private GameManager gameManager;
 
     void Start()
     {
@@ -19,6 +20,7 @@ public class Target : MonoBehaviour
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     Vector3 RandomForce()
@@ -36,9 +38,14 @@ public class Target : MonoBehaviour
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
     private void OnMouseDown()
     {
         Destroy(gameObject); 
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
